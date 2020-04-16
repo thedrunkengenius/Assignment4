5d2b8be49605f8100166b3e096c40e1279650d55
thedrunkengenius <div.patel@gmail.com>
2020-04-16 18:44:07 -0400

    Update CatTest.java

--- CatTest.java
+++ CatTest.java
@@ -1,6 +1,10 @@
 public class CatTest {
     public static void main(String[] args) {
+//Cat[] catArray = new Cat[5];
         Cat ca1 = new Cat("Telly", 3, "happy", "Tabby");
+
+  //ca1=catArray[0];      
+
 
     }
 }
